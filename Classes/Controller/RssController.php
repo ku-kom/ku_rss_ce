ce37b557e31613492da2b14f4968c805b669caab
nanna-dk <nanna@e-nanna.dk>
2022-10-04 15:12:03 +0200

    Setup

--- Classes/Controller/RssController.php
+++ Classes/Controller/RssController.php
@@ -5,32 +5,18 @@ declare(strict_types=1);
 namespace UniversityOfCopenhagen\KuRssCe\Controller;
 
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Http\RequestFactory;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
-use TYPO3\CMS\Core\Pagination\ArrayPaginator;
-use TYPO3\CMS\Core\Pagination\SimplePagination;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use DOMElement;
-use Exception;
-use DOMdocument;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use UniversityOfCopenhagen\KuRssCe\Service\RssAppService;
 
 class RssController extends ActionController
 {
     /**
-     * @var RssAppService
+     * Initiate the RequestFactory.
      */
-    protected $rssAppService;
-
-    /**
-     * @param RssAppService $rssAppService
-     * @noinspection PhpUnused
-     */
-    public function injectRssAppService(RssAppService $rssAppService)
-    {
-        $this->rssAppService = $rssAppService;
+    public function __construct(
+        protected readonly RequestFactory $requestFactory,
+    ) {
     }
 
 
@@ -38,61 +24,65 @@ class RssController extends ActionController
      * Request url and return response to fluid template.
      * @return ResponseInterface
      */
-    public function getFeedAction()
+    public function getFeedAction(): ResponseInterface
     {
-        $feedurl = $this->configurationManager->getContentObject()->data['ku_rss_ce'];
-        $itemsPerPage = $this->configurationManager->getContentObject()->data['ku_rss_items'];
+        $cObjectData = $this->configurationManager->getContentObject()->data;
+        $url = $cObjectData['ku_rss_ce'];
+        $itemsPerPage = $cObjectData['ku_rss_items'];
+        
 
         // Check the template is a valid URL
-        if (false === filter_var($feedurl, FILTER_VALIDATE_URL)) {
-            $message = sprintf('Feed URL is not valid "%s". Update your settings.', $feedurl);
+        if (false === filter_var($url, FILTER_VALIDATE_URL)) {
+            $message = sprintf('Feed URL is not valid "%s". Update your settings.', $url);
             throw new \RuntimeException($message, 1320651278);
         }
 
-        $data = file_get_contents($feedurl);
-        $xml = simplexml_load_string($data);
-      
-
-        $feed = $this->parseData($xml, $itemsPerPage);
-        //debug($feed);
-        
-        // try {
-        //     $feed = $this->fetchData($xml);
-        // } catch (Exception $exception) {
-        //     // Sisplay error message
-        //     $this->addFlashMessage(
-        //         (string)\TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('rss_warningmsg', 'ku_rss_ce'),
-        //         '',
-        //         FlashMessage::ERROR,
-        //         false
-        //     );
-        // }
-
-        $this->view->assign('feed', $feed);
-    }
-
-    protected function parseData($url, $number)
-    {
         if (!empty($url)) {
-            $items = [];
-            $i = 0;
-            foreach($url->channel->item as $entry) {
-                if ($i >= $number) {
-                    break;
+            $response = $this->requestFactory->request($url, 'GET');
+
+            // Get the content on a successful request
+            if ($response->getStatusCode() === 200) {
+
+                $feedtype = '';
+                if (false !== strpos($response->getHeaderLine('Content-Type'), 'application/atom+xml')) {
+                    $feedtype = 'atom';
+                } elseif (false !== strpos($response->getHeaderLine('Content-Type'), 'application/rss+xml')) {
+                    $feedtype = 'rss';
                 }
-                debug($entry);
-                
-                $items[] = [
-                    'link' => $entry->link,
-                    'title' => $entry->title,
-                    'image' => '',
-                    'description' => $entry->description,
-                    'pubDate' => $entry->pubDate
-                ];
-                $i ++;
+       
+                $data = $response->getBody()->getContents();
+                $xml = simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOWARNING | LIBXML_NOERROR | LIBXML_NOCDATA);
+                $json = json_encode($xml);
+                $feed = json_decode($json, true);
+
+                //debug($feed);
+
+                if ($feedtype == 'rss') {
+                    // Feed is RSS 2.0
+                    $items = $feed['channel']['item'];
+                    $this->view->assign('type', 'rss');
+                    $this->view->assign('title', $feed['channel']['title']);
+                    $this->view->assign('description', $feed['channel']['description']);
+                    $this->view->assign('data', $cObjectData);
+                } else {
+                    // Feed is Atom
+                    $items = $feed['entry'];
+                    $this->view->assign('type', 'atom');
+                    $this->view->assign('title', $feed['title']);
+                    $this->view->assign('data', $cObjectData);
+                }
+                $this->view->assign('feed', $items);
+                $this->view->assign('numberOfItems', $itemsPerPage);
+            } else {
+                // Sisplay error message
+                $this->addFlashMessage(
+                    (string)\TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('rss_warningmsg', 'ku_rss_ce'),
+                    '',
+                    FlashMessage::ERROR,
+                    false
+                );
             }
-            return $items;
         }
+        return $this->htmlResponse();
     }
-    
 }
